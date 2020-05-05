1fbac4493a7c8f40146dde9ab291e81dc70a64e7
Hollowstare <Magickpen@yandex.ru>
2020-05-05 14:17:42 +0800

    Add prototypes head.h

---
+++ src/head.h
@@ -0,0 +1,6 @@
+#include <string>
+#pragma once
+std::string CreateWordList_SwapAdjChars(std::string word);
+std::string s(std::string word);
+void CheckFile(std::string filename);
+void ReadDictionaryFile(std::string filename);
